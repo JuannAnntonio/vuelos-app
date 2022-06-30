6768a69800584a7191604ace5423b6a12c2890bb
Juan <dev.juan.perez@gmail.com>
2022-06-30 18:35:56 -0500

    test - bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,16 @@ function App() {
         >
           Learn React
         </a>
+
+        <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+
       </header>
     </div>
   );
