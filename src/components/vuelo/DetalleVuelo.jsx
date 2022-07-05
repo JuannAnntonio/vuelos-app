ea11d39801e174878e97d9540b57dfcc3e40460a
Juan <dev.juan.perez@gmail.com>
2022-07-05 00:25:24 -0500

    Estilos para resultado de Vuelos

--- src/components/vuelo/DetalleVuelo.jsx
+++ src/components/vuelo/DetalleVuelo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Divider } from "primereact/divider";
 import { getAirportByIata, formatTime, formatDateShort } from "./Util";
+import "./DetalleVuelo.css";
 
 export default function DetalleVuelo(props) {
   return (
@@ -11,7 +12,7 @@ export default function DetalleVuelo(props) {
             className="grid flex align-items-center justify-content-center"
             key={"grid" + index}
           >
-            <div className="col-5" style={{textAlign:'center'}}>
+            <div className="col-5" style={{ textAlign: "center" }}>
               <h4>{formatDateShort(segment.departure.at)}</h4>
               <h2>{formatTime(segment.departure.at)}</h2>
               <h4>{segment.departure.iataCode}</h4>
@@ -25,8 +26,10 @@ export default function DetalleVuelo(props) {
                   .name
               }
             </div>
-            <div className="col-2" style={{textAlign:'center'}}>DURACION</div>
-            <div className="col-5" style={{textAlign:'center'}}>
+            <div className="col-2" style={{ textAlign: "center" }}>
+              DURACION
+            </div>
+            <div className="col-5" style={{ textAlign: "center" }}>
               <h4>{formatDateShort(segment.arrival.at)}</h4>
               <h2>{formatTime(segment.arrival.at)}</h2>
               <h4>{segment.arrival.iataCode}</h4>
