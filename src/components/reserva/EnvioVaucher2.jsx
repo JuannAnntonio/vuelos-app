1016fbdcaf9c401650951225329e8a8616b93ba4
Rosalba Fuentes <rosemexico@gmail.com>
2022-07-07 14:20:20 -0500

    changes
    new

--- src/components/reserva/EnvioVaucher2.jsx
+++ src/components/reserva/EnvioVaucher2.jsx
@@ -3,33 +3,54 @@ import { useFormik } from 'formik';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
+import { Calendar } from 'primereact/calendar';
+import { Password } from 'primereact/password';
 import { Checkbox } from 'primereact/checkbox';
 import { Dialog } from 'primereact/dialog';
 import { Divider } from 'primereact/divider';
 import { classNames } from 'primereact/utils';
+import { CountryService } from '../service/CountryService';
 import './FormDemo.css';
 
 export const FormikFormDemo = () => {
+    const [countries, setCountries] = useState([]);
     const [showMessage, setShowMessage] = useState(false);
     const [formData, setFormData] = useState({});
-    
+    const countryservice = new CountryService();
+
     useEffect(() => {
         countryservice.getCountries().then(data => setCountries(data));
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     const formik = useFormik({
         initialValues: {
-            Email (donde recibirás tus vouchers): '',
-            Confirma tu email: '',
+            name: '',
+            email: '',
+            password: '',
+            date: null,
+            country: null,
+            accept: false
         },
         validate: (data) => {
             let errors = {};
+
+            if (!data.name) {
+                errors.name = 'Name is required.';
+            }
 
             if (!data.email) {
                 errors.email = 'Email is required.';
             }
             else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
                 errors.email = 'Invalid email address. E.g. example@email.com';
+            }
+
+            if (!data.password) {
+                errors.password = 'Password is required.';
+            }
+
+            if (!data.accept) {
+                errors.accept = 'You need to agree to the terms and conditions.';
             }
 
             return errors;
@@ -42,29 +63,49 @@ export const FormikFormDemo = () => {
         }
     });
 
-    const isFormFieldValid = (Confirma tu email) => !!(formik.touched[name] && formik.errors[name]);
-    const getFormErrorMessage = (Confirma tu email) => {
-        return isFormFieldValid(Confirma tu email) && <small className="p-error">{formik.errors[Confirma tu email]}</small>;
+    const isFormFieldValid = (name) => !!(formik.touched[name] && formik.errors[name]);
+    const getFormErrorMessage = (name) => {
+        return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
     };
 
-    
+    const dialogFooter = <div className="flex justify-content-center"><Button label="OK" className="p-button-text" autoFocus onClick={() => setShowMessage(false)} /></div>;
+    const passwordHeader = <h6>Pick a password</h6>;
+    const passwordFooter = (
+        <React.Fragment>
+            <Divider />
+            <p className="mt-2">Suggestions</p>
+            <ul className="pl-2 ml-2 mt-0" style={{ lineHeight: '1.5' }}>
+                <li>At least one lowercase</li>
+                <li>At least one uppercase</li>
+                <li>At least one numeric</li>
+                <li>Minimum 8 characters</li>
+            </ul>
+        </React.Fragment>
+    );
 
     return (
         <div className="form-demo">
             <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
                 <div className="flex align-items-center flex-column pt-6 px-3">
                     <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
+                    <h5>Registration Successful!</h5>
+                    <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
+                        Your account is registered under name <b>{formData.name}</b> ; it'll be valid next 30 days without activation. Please check <b>{formData.email}</b> for activation instructions.
+                    </p>
                 </div>
             </Dialog>
 
             <div className="flex justify-content-center">
                 <div className="card">
-                    <h5 className="text-center">
-					¿A dónde enviamos tus vouchers?
-                    El email que elijas será fundamental para que gestiones tu reserva y recibas información importante sobre tu viaje.
-					</h5>
+                    <h5 className="text-center">Register</h5>
                     <form onSubmit={formik.handleSubmit} className="p-fluid">
-                        
+                        <div className="field">
+                            <span className="p-float-label">
+                                <InputText id="name" name="name" value={formik.values.name} onChange={formik.handleChange} autoFocus className={classNames({ 'p-invalid': isFormFieldValid('name') })} />
+                                <label htmlFor="name" className={classNames({ 'p-error': isFormFieldValid('name') })}>Name*</label>
+                            </span>
+                            {getFormErrorMessage('name')}
+                        </div>
                         <div className="field">
                             <span className="p-float-label p-input-icon-right">
                                 <i className="pi pi-envelope" />
@@ -73,45 +114,35 @@ export const FormikFormDemo = () => {
                             </span>
                             {getFormErrorMessage('email')}
                         </div>
-						
-						<div className="field">
+                        <div className="field">
                             <span className="p-float-label">
-                                <InputText id="Confirma tu email" name="Confirma tu email" value={formik.values.name} onChange={formik.handleChange} autoFocus className={classNames({ 'p-invalid': isFormFieldValid('Confirma tu email') })} />
-                                <label htmlFor="Confirma tu email" className={classNames({ 'p-error': isFormFieldValid('Confirma tu email') })}>Confirma tu email*</label>
+                                <Password id="password" name="password" value={formik.values.password} onChange={formik.handleChange} toggleMask
+                                    className={classNames({ 'p-invalid': isFormFieldValid('password') })} header={passwordHeader} footer={passwordFooter} />
+                                <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid('password') })}>Password*</label>
                             </span>
-                            {getFormErrorMessage('name')}
+                            {getFormErrorMessage('password')}
                         </div>
-						
-                                                                     
+                        <div className="field">
+                            <span className="p-float-label">
+                                <Calendar id="date" name="date" value={formik.values.date} onChange={formik.handleChange} dateFormat="dd/mm/yy" mask="99/99/9999" showIcon />
+                                <label htmlFor="date">Birthday</label>
+                            </span>
+                        </div>
+                        <div className="field">
+                            <span className="p-float-label">
+                                <Dropdown id="country" name="country" value={formik.values.country} onChange={formik.handleChange} options={countries} optionLabel="name" />
+                                <label htmlFor="country">Country</label>
+                            </span>
+                        </div>
+                        <div className="field-checkbox">
+                            <Checkbox inputId="accept" name="accept" checked={formik.values.accept} onChange={formik.handleChange} className={classNames({ 'p-invalid': isFormFieldValid('accept') })} />
+                            <label htmlFor="accept" className={classNames({ 'p-error': isFormFieldValid('accept') })}>I agree to the terms and conditions*</label>
+                        </div>
+
+                        <Button type="submit" label="Submit" className="mt-2" />
                     </form>
                 </div>
             </div>
         </div>
     );
 }
-
-
-/* FormDemo.css */
-
-.form-demo .card {
-  min-width: 450px;
-}
-.form-demo .card form {
-  margin-top: 2rem;
-}
-.form-demo .card .field {
-  margin-bottom: 1.5rem;
-}
-@media screen and (max-width: 960px) {
-  .form-demo .card {
-      width: 80%;
-  }
-}
-@media screen and (max-width: 640px) {
-  .form-demo .card {
-      width: 100%;
-      min-width: 0;
-  }
-}
-           
-export default App;
