9744d8b0d1a54177e0ffbc2c4656bbc78af49748
Rosalba Fuentes <rosemexico@gmail.com>
2022-07-12 14:30:08 -0500

    changes file Datos tarjeta

--- src/components/reserva/DatosTarjeta.jsx
+++ src/components/reserva/DatosTarjeta.jsx
@@ -4,11 +4,12 @@ import { InputNumber } from "primereact/inputnumber";
 import { Card } from "primereact/card";
 import { SelectButton } from "primereact/selectbutton";
 import { InputMask } from "primereact/inputmask";
+import { Calendar } from "primereact/calendar";
 
 export default function DatosTarjeta() {
   const [pago, setPago] = useState(null);
   const optionPago = ["Tarjeta de crédito", "Tarjeta de débito"];
-  const [vencimiento, setVencimiento]= ["Mm/yy"];
+  const [date9, setDate9] = useState(null);
  
   return (
     <Card>
@@ -45,13 +46,14 @@ export default function DatosTarjeta() {
             <InputText placeholder="Titular de la Tarjeta" />
           </div>
         </div>
-        <div className="col-12" style={{ fontSize: "18px" }}>
-          <div className="p-inputgroup">
-            <span className="p-inputgroup-addon">
-              <i className="pi pi-user"></i>
-            </span>
-            <InputMask placeholder="Vencimiento Mm/yy" />
-          </div>
+        <div
+          className="col-3 flex align-items-center"
+          style={{ fontSize: "17px" }}
+        >
+          Fecha de Vencimiento:
+        </div>
+        <div className="col-9" style={{ fontSize: "18px" }}>
+            <Calendar id="monthpicker" value={date9} onChange={(e) => setDate9(e.value)} view="month" dateFormat="mm/yy" showIcon />
         </div>
 
         <div className="col-12" style={{ fontSize: "18px" }}>
