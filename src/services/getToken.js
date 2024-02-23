48dd1f394949abfa1c84b6d047cb3368ad8e6ed8
guzmanpdro <pedroguzman2598@gmail.com>
2024-02-23 00:21:47 -0400

    Fix visible environment variables

--- src/services/getToken.js
+++ src/services/getToken.js
@@ -1,12 +1,14 @@
 const tokenURL = "https://test.api.amadeus.com/v1/security/oauth2/token"
+const apiKey = process.env.REACT_APP_API_KEY
+const apiSecret = process.env.REACT_APP_API_SECRET
 
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
 const urlencoded = new URLSearchParams();
 urlencoded.append("grant_type", "client_credentials");
-urlencoded.append("client_id", "DoCADaDFlpifAhLG9JENmnNwNzWf0DgW");
-urlencoded.append("client_secret", "VHjjFfVXEGpKHmh1");
+urlencoded.append("client_id", apiKey);
+urlencoded.append("client_secret", apiSecret);
 
 export async function getToken() {
   const data = await fetch(tokenURL, {
